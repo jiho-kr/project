eb050ebae07403ed83cdee76ad9501f18907b045
jiho-kr <jipark@ea.com>
2020-10-13 14:58:34 +0900

    Create CustomBar.vue

---
+++ vue/components/ChartJs/CustomBar.vue
@@ -0,0 +1,23 @@
+<script>
+import { Bar } from 'vue-chartjs';
+import 'chartjs-plugin-colorschemes';
+import 'chartjs-plugin-datalabels';
+
+export default {
+  extends: Bar,
+  props: ['data', 'options'],
+  methods: {
+    renderBarChart() {
+      this.renderChart(this.data, this.options);
+    }
+  },
+  watch: {
+    data: function() {
+      this.renderBarChart();
+    }
+  },
+  mounted() {
+    this.renderBarChart();
+  }
+}
+</script>
